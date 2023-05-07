8bfaef3d430416cfc5966ed5eb9912cf59d89365
tsopsericado <ricardotsopse@gmail.com>
2023-05-07 03:23:29 +0100

    some features added in the js file

--- index.js
+++ index.js
@@ -9,5 +9,41 @@ var denom = [
   { name: "NICKEL", value: 0.05 },
   { name: "PENNY", value: 0.01 },
 ];
+function checkCashRegister(price, cash, cid) {
+  var change = cash - price;
+  let result = {
+    status: "",
+    change: [],
+  };
+  let sumCid = 0;
+  for (let i = 0; i < cid.length; i++) {
+    sumCid += cid[i][1];
+  }
+  if (sumCid < cid) {
+    result.status = "INSUFFICIENT_FUNDS";
+    return result;
+  }
+  if (sumCid === change) {
+    result.status = "CLOSED";
+    result.change = cid;
+    return change;
+  }
 
+  if (sumCid > change) {
+    result.status = "[]";
+  }
+
+  console.log("sumCid", sumCid);
+  let newResult = [];
+
+  cid = cid.reverse();
+  for (i = 0; i < cid.length; i++) {
+    let val = 0;
+    while (denom[i].value <= change && cid[i][1] > 0) {
+      cid[i][1] -= denom[i].value;
+      change -= denom[i].value;
+      val += denom[i].value;
+    }
+  }
+  return change;
 }
